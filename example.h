2e617c3b9ebdc4a9a084fe0b7db4610e8b645127
kumanoff <mayeriegor@yandex.ru>
2023-02-08 11:23:38 +0700

    first commit

---
+++ example.h
@@ -0,0 +1,16 @@
+//#pragma once
+//int sumOfTwoNumbers(int a, int b);
+/*#ifndef GYGY_head
+#define GYGY_head
+#include <string.h>
+int sumOfTwoNumbers(int a, int b)
+{
+    return (a + b);
+}
+#endif*/
+#pragma once//defining for one time 
+#include <string.h>
+int sumOfTwoNumbers(int a, int b)
+{
+    return (a + b);
+}
---
+++ main.c
@@ -0,0 +1,15 @@
+// Including header file
+#include "example.h"
+#include "example.h"
+#include <stdio.h>
+sumOfTwoNumbers(int a, int b);
+// Driver Code
+int main()
+{
+ 
+    // Given two numbers 
+    int a = 13, b = 22;
+    int sum = sumOfTwoNumbers(a, b);
+    printf("sum = %d \n", sum); 
+    return 0; 
+}
